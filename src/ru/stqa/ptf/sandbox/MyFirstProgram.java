afe930d5f49a40b905b14ce85de5dc04f4bf6b7c
sloymad <sloymad@gmail.com>
2018-02-08 12:13:13 +0300

    Пакет заработал
    Пакет заработал

--- src/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ src/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -1,9 +1,8 @@
 package ru.stqa.ptf.sandbox;
 
-class MyFirstProgram {
+public class MyFirstProgram {
 
     public static void main(String[] args) {
-
         System.out.println("Hello, world");
     }
 }
